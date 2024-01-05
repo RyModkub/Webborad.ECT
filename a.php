bce1750b7b9506f71b9235dfb7d929ee40dc038b
rymodkub <rymodkub@gmail.com>
2024-01-05 08:43:51 +0700

    Ex2 in php1
    Ex2 in php1

---
+++ a.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php  
+    $a =4 ;
+    $b =10 ;
+    $c = ($b-5) * $a;
+    echo "result 1 =" .$c. "<BR>";
+    echo 'result 2 =' .$c. '<BR>';
+    echo "result 3 =  $c <BR>";
+    echo 'result 4 =  $c <BR>';
+    echo "result 5 =  $c + $b <BR>";
+    ?>
+</body>
+</html>
