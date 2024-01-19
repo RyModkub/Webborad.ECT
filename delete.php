e47b74a9f768290c288a37a0bacfc7e0d17a875f
rymodkub <rymodkub@gmail.com>
2024-01-19 11:22:41 +0700

    refresh delete.php
    refresh delete.php

--- delete.php
+++ delete.php
@@ -3,6 +3,7 @@ session_start();
 if(isset($_SESSION['id']) && $_SESSION['role']=='a') {
     $id = ($_GET["id"]);
     echo "ลบกระทู้หมายเลข $id";
+    header("Refresh:1; url=index.php");
 }
 else 
 header("Location: index.php");
